6efd584d04b1b4c289ab8cdaf7fba57c247ea2ac
JINZHANG2017 <zj574406254@hotmail.com>
2020-10-16 21:36:26 +0800

    [ZhangJin] feat : finish tests

--- src/mock/class/__test__/vaccineTest.test.js
+++ src/mock/class/__test__/vaccineTest.test.js
@@ -6,6 +6,14 @@ const mockAcceptInjection = jest.fn();
 
 jest.mock("../recipient", () => {
   // mock class实现
+  // mockAcceptInjection();
+  const theMockOfClass = jest.fn().mockImplementation(() => {
+    const returnValue = {
+      acceptInjection: mockAcceptInjection,
+    };
+    return returnValue;
+  });
+  return theMockOfClass;
 });
 
 beforeEach(() => {
@@ -17,9 +25,14 @@ describe("inject", () => {
   test("should recipient accept injection with vaccine", () => {
     // Arrange
     // 创建VaccineTest
+    const vtest = new VaccineTest();
     // Act
     // 调用inject
+    vtest.inject();
     // Assert
     // 判断模拟recipient对象有没有接收注射，及模拟acceptInjection被调用
+    expect(mockAcceptInjection).toHaveBeenCalledWith(
+      expect.any(Covid19Vaccine)
+    );
   });
 });
